ac59e5d4ad295809cdc267c657625aab4314def5
焦质晔 <jiaozy@yonyou.com>
2019-09-29 14:51:23 +0800

    修改了ajaxBtn组件

--- src/components/AjaxButton/AjaxButton.vue
+++ src/components/AjaxButton/AjaxButton.vue
@@ -1,4 +1,10 @@
 <script>
+/**
+ * @Author: 焦质晔
+ * @Date: 2019-05-06 10:00:00
+ * @Last Modified by:   焦质晔
+ * @Last Modified time: 2019-05-07 11:00:00
+ **/
 export default {
   name: 'AjaxButton',
   props: {
