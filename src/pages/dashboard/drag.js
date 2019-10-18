8ecf681d71a702b4c598d0151cf8e6637a4b6bb0
jiaozhiye <jiaozhiye@163.com>
2019-10-18 23:44:39 +0800

    qwe

--- src/pages/dashboard/drag.js
+++ src/pages/dashboard/drag.js
@@ -6,6 +6,8 @@
  */
 
 export default class DragElement {
+  targetNode = null;
+
   constructor(props) {
     this.createParams(props);
     this.initalHandle();
@@ -41,6 +43,14 @@ export default class DragElement {
     return res.slice(0, 2).map(x => Number(x));
   };
 
+  setStyleHandle = (obj, prefixFeature, value) => {
+    let prefix = prefixFeature.slice(0, 1).toUpperCase() + prefixFeature.slice(1);
+    let prefixArr = ['webkit' + prefix, 'moz' + prefix, 'ms' + prefix, prefixFeature];
+    for (let i = 0; i < prefixArr.length; i++) {
+      obj.style[prefixArr[i]] = value;
+    }
+  };
+
   resetPosition = () => {
     this.$dragNode.style.transition = 'all 0.3s ease';
     this.moveHandle(0, 0);
@@ -48,8 +58,64 @@ export default class DragElement {
     setTimeout(() => (this.$dragNode.style.transition = null), 300);
   };
 
+  // el1 移动节点     el2 目标节点
+  isCollision = (el1, el2) => {
+    const l1 = el1.getBoundingClientRect().left,
+      r1 = l1 + el1.offsetWidth,
+      t1 = el1.getBoundingClientRect().top,
+      b1 = t1 + el1.offsetHeight;
+    const l2 = el2.getBoundingClientRect().left,
+      r2 = l2 + el2.offsetWidth,
+      t2 = el2.getBoundingClientRect().top,
+      b2 = t2 + el2.offsetHeight;
+    if (l1 > r2 || r1 < l2 || t1 > b2 || b1 < t2) {
+      // 没有撞上
+      return false;
+    }
+    // 撞上了
+    return true;
+  };
+
+  calcLength(el1, el2) {
+    const { left: l1, top: t1 } = el1.getBoundingClientRect();
+    const { left: l2, top: t2 } = el2.getBoundingClientRect();
+    const { offsetWidth: w1, offsetHeight: h1 } = el1;
+    const { offsetWidth: w2, offsetHeight: h2 } = el2;
+    const disX = Math.abs(l1 + w1 / 2 - (l2 + w2 / 2));
+    const disY = Math.abs(t1 + h1 / 2 - (t2 + h2 / 2));
+    return Math.sqrt(disX * disX + disY * disY);
+  }
+
+  findTargetNode = el => {
+    const res = [];
+    for (let i = 0; i < this.targetNodes.length; i++) {
+      const target = this.targetNodes[i];
+      if (this.isCollision(el, target)) {
+        res.push(target);
+      }
+    }
+    if (!res.length) return null;
+    const arr = res.map(x => ({ node: x, val: this.calcLength(el, x) })).sort((a, b) => a.val - b.val);
+    return arr[0].node;
+  };
+
+  setTableCellClass = el => {
+    for (let i = 0; i < this.targetNodes.length; i++) {
+      this.targetNodes[i].classList.remove('actived');
+    }
+    el && el.classList.add('actived');
+  };
+
   moveHandle = (l, t) => {
-    this.$dragNode.style.transform = `translate3d(${l}px, ${t}px, 0)`;
+    this.setStyleHandle(this.$dragNode, 'transform', `translate3d(${l}px, ${t}px, 0)`);
+    const target = this.findTargetNode(this.$dragNode);
+    this.targetNode = null;
+    if (target && !target.children.length) {
+      this.targetNode = target;
+      this.setTableCellClass(target);
+    } else {
+      this.setTableCellClass();
+    }
   };
 
   createDragHandle = () => {
@@ -57,8 +123,8 @@ export default class DragElement {
       this.$dragNode.style.zIndex = 1;
 
       const arr = this.getTranslateValue(this.$dragNode);
-      let disX = ev.clientX - arr[0];
-      let disY = ev.clientY - arr[1];
+      const disX = ev.clientX - arr[0];
+      const disY = ev.clientY - arr[1];
 
       document.onmousemove = e => {
         let l = e.clientX - disX;
@@ -67,9 +133,13 @@ export default class DragElement {
       };
 
       document.onmouseup = () => {
+        this.resetPosition();
+        this.setTableCellClass();
+        if (this.targetNode) {
+          this.callback && this.callback(this.$dragNode, this.targetNode);
+        }
         document.onmousemove = null;
         document.onmouseup = null;
-        this.resetPosition();
       };
 
       return false;
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -12,21 +12,31 @@ export default {
   name: 'Dashboard',
   data() {
     this.tHeadsList = ['序号', '销售1', '销售2', '销售3', '销售4', '销售5'];
+    this.dataList = [
+      {
+        id: '1',
+        name: '张三',
+        imgUrl: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+        num: 10,
+        time: '12:12:12'
+      },
+      {
+        id: '2',
+        name: '李四',
+        imgUrl: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+        num: 20,
+        time: '15:15:15'
+      },
+      {
+        id: '3',
+        name: '刘德华',
+        imgUrl: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
+        num: 50,
+        time: '05:05:05'
+      }
+    ];
     return {
-      workers: [
-        {
-          id: '1',
-          name: '张三',
-          imgUrl: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-          num: 10
-        },
-        {
-          id: '2',
-          name: '李四',
-          imgUrl: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg',
-          num: 20
-        }
-      ],
+      workers: [...this.dataList],
       list: this.createTableList()
     };
   },
@@ -36,33 +46,57 @@ export default {
       for (let i = 0; i < 10; i++) {
         let tmp = [];
         this.tHeadsList.forEach(x => {
-          tmp.push({});
+          tmp.push(null);
         });
         res.push(tmp);
       }
       return res;
     },
-    createWorkerNode(item) {
+    createWorkerNode(item, pos = []) {
       return (
-        <div class="worker">
-          <div class="info">
-            <span class="img">
-              <img src={item.imgUrl} />
+        <div class="worker" data-id={item.id}>
+          <span class="img">
+            <img src={item.imgUrl} />
+          </span>
+          <span class="name">{item.name}</span>
+          {pos.length ? <span class="time-len">{item.time}</span> : null}
+          {pos.length ? (
+            <span class="del-btn" onClick={() => this.removeHandle(item.id, pos)}>
+              <i class="el-icon-delete"></i>
             </span>
-            <span class="name">{item.name}</span>
-          </div>
-          <div class="box">数量：{item.num}</div>
+          ) : null}
         </div>
       );
     },
-    createCellNode(item) {
-      if (!Object.keys(item).length) return null;
-      return <div>asdasd</div>;
+    removeHandle(id, [x, y]) {
+      this.list[x][y] = null;
+      this.workers.push(this.dataList.find(x => x.id == id));
+      this.list = [...this.list];
+      this.$nextTick(() => {
+        // this.bindDragEvent();
+      });
+    },
+    createTableCellNode(item, pos) {
+      if (!item) return null;
+      return this.createWorkerNode(item, pos);
+    },
+    getTableTdsHandle() {
+      const tableTds = Array.from(document.querySelectorAll('.table-wrap > tbody tr td'));
+      return tableTds.filter(x => !x.getAttribute('disabled'));
     },
     bindDragEvent() {
-      const nodes = Array.from(this.$refs.workerList.querySelectorAll('.worker'));
-      nodes.forEach(x => {
-        new DragElement({ $dragNode: x, targetNodes: [] });
+      const targets = Array.from(this.$refs.workerList.querySelectorAll('.worker'));
+      targets.forEach(x => {
+        new DragElement({
+          $dragNode: x,
+          targetNodes: this.getTableTdsHandle(),
+          callback: (el, target) => {
+            const index = this.workers.findIndex(x => x.id === el.getAttribute('data-id'));
+            this.list[target.getAttribute('x')][target.getAttribute('y')] = this.workers[index];
+            this.workers.splice(index, 1);
+            this.list = [...this.list];
+          }
+        });
       });
     }
   },
@@ -80,7 +114,7 @@ export default {
             <thead>
               <tr>
                 {this.tHeadsList.map((x, index) => (
-                  <th key={x} width={index === 0 ? 40 : null}>
+                  <th key={x} width={index === 0 ? null : '19%'}>
                     {x}
                   </th>
                 ))}
@@ -91,11 +125,15 @@ export default {
                 <tr key={`row-${index}`}>
                   {arr.map((x, i) => {
                     if (i === 0) {
-                      return <td disabled>{index + 1}</td>;
+                      return (
+                        <td disabled>
+                          <span style={{ paddingLeft: '10px' }}>{index + 1}</span>
+                        </td>
+                      );
                     }
                     return (
-                      <td x={index} y={i}>
-                        {this.createCellNode(x)}
+                      <td x={index} y={i} disabled={!!x ? 'disabled' : null}>
+                        {this.createTableCellNode(x, [index, i])}
                       </td>
                     );
                   })}
@@ -112,68 +150,77 @@ export default {
 
 <style scoped lang="less">
 .worker {
-  border: 1px solid @borderColorSecondary;
-  border-radius: @borderRadius;
+  min-width: 80px;
   padding: @modulePadding;
-  box-shadow: @boxShadow;
-  background-color: #fff;
   position: relative;
-  .info {
-    .img {
-      display: inline-block;
-      overflow: hidden;
-      width: 24px;
-      height: 24px;
-      border-radius: @borderRadius;
-      vertical-align: middle;
-      img {
-        object-fit: cover;
-        display: block;
-        height: 100%;
-      }
-    }
-    .name {
-      display: inline-block;
-      margin-left: 5px;
-      line-height: 24px;
-      vertical-align: middle;
+  .img {
+    display: inline-block;
+    overflow: hidden;
+    width: 24px;
+    height: 24px;
+    border-radius: @borderRadius;
+    img {
+      object-fit: cover;
+      display: block;
+      height: 100%;
     }
   }
-  .box {
-    margin-top: 7px;
-    padding-top: 5px;
-    border-top: 1px solid @borderColorSecondary;
+  .name {
+    margin-left: 6px;
+    line-height: 24px;
+  }
+  .time-len {
+    margin-left: @moduleMargin;
+  }
+  .del-btn {
+    position: absolute;
+    padding: 5px;
+    right: 5px;
+    cursor: pointer;
+    &:hover {
+      color: @primaryColor;
+    }
+  }
+  & > * {
+    display: inline-block;
+    vertical-align: middle;
   }
 }
 .worker-list {
+  min-height: 46px;
   display: flex;
   justify-content: flex-start;
   flex-wrap: wrap;
   position: relative;
   .worker {
-    min-width: 80px;
+    border: 1px solid @borderColorSecondary;
+    border-radius: @borderRadius;
     margin-right: @moduleMargin;
+    box-shadow: @boxShadow;
+    background-color: #fff;
     cursor: move;
   }
 }
 .plan-container {
-  margin-top: 10px;
+  margin-top: @moduleMargin;
   .table-wrap {
     width: 100%;
     border: 1px solid @borderColorSecondary;
     thead tr th {
       text-align: left;
       font-weight: 700;
-      padding: 10px 5px;
+      padding: 10px 0 10px 10px;
       border: 1px solid @borderColorSecondary;
       background-color: @backgroundColor;
       vertical-align: middle;
     }
     tbody tr td {
-      padding: 5px;
-      height: 30px;
+      height: 46px;
       border: 1px solid @borderColorSecondary;
       vertical-align: middle;
+      &.actived {
+        background-color: @backgroundColorSecondary;
+      }
     }
   }
 }
