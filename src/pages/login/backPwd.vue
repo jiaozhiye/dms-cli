21e6fcdc4602e1ed90fb0060e18cec8ea3d5c191
焦质晔 <jiaozy@yonyou.com>
2019-10-23 13:09:02 +0800

    修改了登录页面

---
+++ src/pages/login/backPwd.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="wrapper">
+    <div class="scroll">
+      <div class="box">
+        <el-form ref="form-phone" size="medium" :model="form" :rules="rules">
+          <el-form-item prop="phone">
+            <el-input v-model="form.phone" prefix-icon="el-icon-mobile-phone" placeholder="请输入手机号" />
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              type="password"
+              class="fl"
+              v-model="form.vcode"
+              placeholder="验证码"
+              style="width: 50%"
+              prefix-icon="el-icon-message"
+            />
+            <el-button class="fr" style="width: 38%">获取验证码</el-button>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" size="medium" :disabled="!isNextStep" @click="nextHandle">下一步</el-button>
+      </div>
+      <div class="box">
+        <el-input v-model="newPassword" prefix-icon="el-icon-lock" placeholder="重置密码" />
+        <el-button type="primary">确 定</el-button>
+        <el-button type="text" class="btn" @click="closeHandle">返回首页</el-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { phone } from '@/utils/validate';
+
+export default {
+  name: '',
+  data() {
+    return {
+      form: {
+        phone: '',
+        vcode: ''
+      },
+      newPassword: '',
+      rules: {
+        phone: [{ required: true, validator: phone, trigger: 'blur' }]
+      },
+      isNextStep: false
+    };
+  },
+  methods: {
+    nextHandle() {},
+    closeHandle(val) {
+      this.$emit('close', val);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.wrapper {
+  overflow: hidden;
+  .scroll {
+    width: 200%;
+    min-height: 160px;
+    display: flex;
+    transition: transform 0.3s ease;
+    .box {
+      width: 50%;
+      /deep/ .el-form-item {
+        margin-bottom: 20px;
+      }
+      /deep/ .el-input__inner {
+        font-size: 14px;
+        border: 0;
+        border-bottom: 1px solid #d6d6d6 !important;
+      }
+      /deep/ .el-input__icon {
+        font-size: 18px;
+      }
+      .btn {
+        font-size: 14px;
+        margin-top: 20px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="login">
-    <div class="left"></div>
+    <div class="left">
+      <div class="logo-wrap">
+        <span>
+          <img src="@/assets/img/logo_yq.png" width="120" />
+        </span>
+        <span>
+          <img src="@/assets/img/logo_audi.png" width="240" />
+        </span>
+      </div>
+      <div class="copy-wrap">
+        <img src="@/assets/img/login_copy.png" width="640" />
+      </div>
+      <div class="btn-wrap">
+        <a href="javascript:;" class="audi-button">
+          <span>了解详情</span>
+          <i class="el-icon-right"></i>
+        </a>
+      </div>
+    </div>
     <div class="right">
       <div class="wrap">
         <h3>
@@ -8,39 +26,47 @@
         </h3>
         <div class="main">
           <div class="top">
-            <span
-              v-for="item in labels"
-              :class="{ actived: currentMark === item.value }"
-              :key="item.value"
-              @click="clickHandler(item.value)"
-            >{{ item.text }}</span>
+            <template v-for="(item, index) in labels">
+              <span
+                :class="{ actived: currentMark === item.value }"
+                :key="item.value"
+                @click="clickHandler(item.value)"
+              >{{ item.text }}</span>
+              <i :key="index" v-if="index !== labels.length - 1">|</i>
+            </template>
           </div>
           <div class="container">
             <div class="scroll" :style="scrollTranslate">
               <div class="box">
-                <el-form ref="form-user" size="medium" :rules="rules">
+                <el-form ref="form-user" size="medium" :model="form" :rules="rules">
                   <el-form-item prop="username">
                     <el-input
                       v-model="form.username"
                       prefix-icon="el-icon-user"
                       placeholder="请输入用户名"
+                      auto-complete="on"
                     />
                   </el-form-item>
                   <el-form-item prop="password">
                     <el-input
-                      type="password"
                       v-model="form.password"
+                      :type="passwordType"
                       placeholder="请输入密码"
                       prefix-icon="el-icon-lock"
+                      auto-complete="on"
+                      @keyup.enter.native="loginHandle"
                     />
+                    <span class="show-pwd" @click="showPwdHandle">
+                      <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
+                    </span>
                   </el-form-item>
                 </el-form>
                 <div class="forget">
-                  <a href="/">忘记密码</a>
+                  <a href="javascript:;" @click="forgetPwdHandle">忘记密码</a>
                 </div>
               </div>
               <div class="box">
-                <el-form ref="form-phone" size="medium" :rules="rules">
+                <el-form ref="form-phone" size="medium" :model="form" :rules="rules">
                   <el-form-item prop="phone">
                     <el-input
                       v-model="form.phone"
@@ -73,26 +99,77 @@
           </div>
           <div class="footer">
             <div class="quick">
-              <span>微信登录</span>
-              <span>APP下载</span>
+              <span @click="showDialogHandle('Wx')">
+                <i class="iconfont icon-weixin"></i> 微信登录
+              </span>
+              <span @click="showDialogHandle('App')">
+                <i class="iconfont icon-appxiazai"></i>APP下载
+              </span>
             </div>
           </div>
         </div>
       </div>
     </div>
+    <BaseDialog
+      :visible.sync="actionWx.visible"
+      :title="actionWx.title"
+      width="500px"
+      destroyOnClose
+    >
+      <div class="tc">
+        <img :src="wxLoginQrcode" width="300" />
+      </div>
+      <div class="tc" style="padding-bottom: 15px;">{{ actionApp.desc }}</div>
+    </BaseDialog>
+    <BaseDialog
+      :visible.sync="actionApp.visible"
+      :title="actionApp.title"
+      width="500px"
+      destroyOnClose
+    >
+      <div class="tc">
+        <img :src="appDevQrcode" width="300" />
+      </div>
+      <div class="tc" style="padding-bottom: 15px;">{{ actionApp.desc }}</div>
+    </BaseDialog>
+    <BaseDialog
+      :visible.sync="actionPwd.visible"
+      :title="actionPwd.title"
+      width="500px"
+      destroyOnClose
+    >
+      <backPwd @close="closePwdBackHandle" />
+    </BaseDialog>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex';
 import { sleep } from '@/utils';
+import { phone } from '@/utils/validate';
 import { doLogin } from '@/api/login';
+
+import backPwd from './backPwd.vue';
+
+// app 在 dev 和 prod 环境的下载二维码
+const appDevQrcode = require('@/assets/img/app_dev_qrcode.png');
+const appProdQrcode = require('@/assets/img/app_prod_qrcode.png');
+
+// 微信登录的二维码
+const wxLoginQrcode = require('@/assets/img/wx_qrcode.png');
 
 export default {
   name: 'Login',
+  components: {
+    backPwd
+  },
   data() {
     // tab 标签
     this.labels = [{ text: '用户名登录', value: 'user' }, { text: '手机号登录', value: 'phone' }];
+    // app 二维码
+    this.appDevQrcode = appDevQrcode;
+    this.appProdQrcode = appProdQrcode;
+    this.wxLoginQrcode = wxLoginQrcode;
     return {
       form: {
         username: '',
@@ -103,9 +180,24 @@ export default {
       rules: {
         username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
         password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
-        phone: [{ required: true, message: '请输入用户名', trigger: 'blur' }]
+        phone: [{ required: true, validator: phone, trigger: 'blur' }]
       },
-      currentMark: 'user' // 当前标记
+      passwordType: 'password',
+      currentMark: 'user', // 当前标记
+      actionWx: {
+        title: '微信登录',
+        visible: false,
+        desc: '请使用微信扫描二维码登录'
+      },
+      actionApp: {
+        title: '下载APP',
+        visible: false,
+        desc: '请扫描二维码下载奥迪销售助手APP'
+      },
+      actionPwd: {
+        title: '密码找回',
+        visible: false
+      }
     };
   },
   computed: {
@@ -121,6 +213,22 @@ export default {
     ...mapActions('app', ['createLoginInfo']),
     clickHandler(type) {
       this.currentMark = type;
+    },
+    showPwdHandle() {
+      if (this.passwordType === 'password') {
+        this.passwordType = '';
+      } else {
+        this.passwordType = 'password';
+      }
+    },
+    showDialogHandle(type) {
+      this[`action${type}`].visible = true;
+    },
+    forgetPwdHandle() {
+      this.actionPwd.visible = true;
+    },
+    closePwdBackHandle(val) {
+      this.actionPwd.visible = false;
     },
     async loginHandle() {
       const res = await doLogin({
@@ -152,6 +260,42 @@ export default {
     flex: 1;
     background: url(../../assets/img/login_left_bg.jpg) 100% 0 no-repeat;
     background-size: cover;
+    .logo-wrap {
+      display: flex;
+      padding: 8vh 10vh 0;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .copy-wrap {
+      padding: 15vh 10vh 0;
+    }
+    .btn-wrap {
+      padding: 6vh 10vh 0;
+    }
+    .audi-button {
+      display: inline-block;
+      width: 240px;
+      height: 46px;
+      border: 1px solid #fff;
+      border-radius: 2px;
+      line-height: 46px;
+      text-align: center;
+      font-size: 16px;
+      color: #fff;
+      transition: all 0.3s ease;
+      span {
+        margin-right: 5px;
+        vertical-align: middle;
+      }
+      i {
+        transform: scale(1.3, 1);
+        vertical-align: middle;
+      }
+      &:hover {
+        background-color: @primaryColor;
+        border-color: @primaryColor;
+      }
+    }
   }
   .right {
     width: 34vw;
@@ -160,6 +304,7 @@ export default {
       .main {
         padding-top: 10vh;
         .top {
+          display: flex;
           color: @textColorSecondary;
           span {
             padding: 6px;
@@ -167,6 +312,10 @@ export default {
             &.actived {
               color: @primaryColor;
             }
+          }
+          i {
+            font-size: 13px;
+            padding: 6px 0;
           }
         }
         .container {
@@ -191,12 +340,20 @@ export default {
               /deep/ .el-input__icon {
                 font-size: 18px;
               }
+              .show-pwd {
+                position: absolute;
+                top: 0;
+                right: 10px;
+                font-size: 16px;
+                color: @textColorSecondary;
+                cursor: pointer;
+                user-select: none;
+              }
               .forget {
                 margin-top: -5px;
                 text-align: right;
                 a {
                   font-size: 13px;
-                  transition: all 0.3s ease;
                   &:hover {
                     color: @primaryColor;
                   }
@@ -215,8 +372,16 @@ export default {
           .quick {
             line-height: 30px;
             span {
+              line-height: 20px;
               margin-right: 20px;
               cursor: pointer;
+              .iconfont {
+                font-size: 18px;
+                vertical-align: middle;
+              }
+              &:hover {
+                color: @primaryColor;
+              }
             }
           }
         }
