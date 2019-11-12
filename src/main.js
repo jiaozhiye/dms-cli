a7d729e31389fb47a605f2a49685656e6e0ccdd5
焦质晔 <jiaozy@yonyou.com>
2019-11-12 19:54:55 +0800

    优化了架构代码

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: 焦质晔
  * @Date: 2019-06-20 10:00:00
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2019-11-12 13:29:22
+ * @Last Modified time: 2019-11-12 19:44:32
  */
 import Vue from 'vue';
 import App from './App.vue';
