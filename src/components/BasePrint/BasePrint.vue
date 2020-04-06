612f0dbede1ed9262ad7902613fae70723643102
jiaozhiye <jiaozy@yonyou.com>
2020-04-06 16:11:49 +0800

    1234

--- src/components/BasePrint/BasePrint.vue
+++ src/components/BasePrint/BasePrint.vue
@@ -3,7 +3,7 @@
  * @Author: 焦质晔
  * @Date: 2019-06-20 10:00:00
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2020-04-06 14:59:12
+ * @Last Modified time: 2020-04-06 16:02:12
  **/
 import { getLodop } from './LodopFuncs';
 import css from './assets/style.module.js';
@@ -152,10 +152,10 @@ export default {
       });
     },
     createPreviewNodes(_html_) {
-      const $target = document.body.querySelector('.v-preview--wrapper');
+      const $target = document.body.querySelector('.v-lodop-print--preview');
       if (!$target) {
         let $wrapper = document.createElement('div');
-        $wrapper.className = 'v-preview--wrapper';
+        $wrapper.className = 'v-lodop-print--preview';
         $wrapper.innerHTML = _html_;
         document.body.appendChild($wrapper);
         $wrapper = null;
@@ -170,17 +170,17 @@ export default {
   },
   render(h) {
     const vNode = this.state === 'start' ? this.createPrintComponent(h) : null;
-    return <div class="v-print--wrapper">{vNode}</div>;
+    return <div class="v-lodop-print">{vNode}</div>;
   }
 };
 </script>
 
 <style lang="less">
-.v-print--wrapper {
+.v-lodop-print {
   display: none;
   visibility: hidden;
 }
-.v-preview--wrapper {
+.v-lodop-print--preview {
   width: 960px;
   padding: 10px;
   border: 1px solid @borderColor;
