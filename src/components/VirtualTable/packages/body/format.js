a9cca9626b4f8ac3d8ed91571c5ba49af8e4936c
jiaozhiye <jiaozy@yonyou.com>
2020-04-09 13:19:26 +0800

    1234

--- src/components/VirtualTable/packages/body/format.js
+++ src/components/VirtualTable/packages/body/format.js
@@ -2,29 +2,31 @@
  * @Author: 焦质晔
  * @Date: 2020-03-23 12:51:24
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2020-03-25 22:50:12
+ * @Last Modified time: 2020-04-09 13:14:43
  */
 import moment from 'moment';
 import { formatNumber } from '../utils';
 
 const formatMixin = {
-  dateFormat(val) {
-    return moment(val).format('YYYY-MM-DD');
-  },
-  datetimeFormat(val) {
-    return moment(val).format('YYYY-MM-DD HH:mm:ss');
-  },
-  financeFormat(val) {
-    return formatNumber(val);
-  },
-  [`secret-nameFormat`](val) {
-    return val.replace(/^([\u4e00-\u9fa5]{1}).+$/, '$1**');
-  },
-  [`secret-phoneFormat`](val) {
-    return val.replace(/^(\d{3}).+(\d{4})$/, '$1****$2');
-  },
-  [`secret-IDnumber`](val) {
-    return val.replace(/^(\d{3}).+(\w{4})$/, '$1***********$2');
+  methods: {
+    dateFormat(val) {
+      return moment(val).format('YYYY-MM-DD');
+    },
+    datetimeFormat(val) {
+      return moment(val).format('YYYY-MM-DD HH:mm:ss');
+    },
+    financeFormat(val) {
+      return formatNumber(val);
+    },
+    [`secret-nameFormat`](val) {
+      return val.replace(/^([\u4e00-\u9fa5]{1}).+$/, '$1**');
+    },
+    [`secret-phoneFormat`](val) {
+      return val.replace(/^(\d{3}).+(\d{4})$/, '$1****$2');
+    },
+    [`secret-IDnumber`](val) {
+      return val.replace(/^(\d{3}).+(\w{4})$/, '$1***********$2');
+    }
   }
 };
 
--- src/components/VirtualTable/packages/body/index.js
+++ src/components/VirtualTable/packages/body/index.js
@@ -2,7 +2,7 @@
  * @Author: 焦质晔
  * @Date: 2020-02-28 23:01:43
  * @Last Modified by: 焦质晔
- * @Last Modified time: 2020-04-09 13:10:09
+ * @Last Modified time: 2020-04-09 13:18:16
  */
 import addEventListener from 'add-dom-event-listener';
 import { parseHeight, getCellValue, contains } from '../utils';
@@ -10,6 +10,7 @@ import config from '../config';
 import _ from 'lodash';
 
 import formatMixin from './format';
+import keyboardMixin from './keyboard';
 
 import Expandable from '../expandable';
 import Selection from '../selection';
@@ -26,7 +27,7 @@ export default {
       $$body: this
     };
   },
-  mixins: [formatMixin],
+  mixins: [keyboardMixin, formatMixin],
   data() {
     this.prevST = 0;
     this.prevSL = 0;
@@ -99,50 +100,6 @@ export default {
     documentClickEvent({ target }) {
       if (contains(this.$vTableBody, target)) return;
       this.setClickedValues([]);
-    },
-    documentKeyboardEvent(ev) {
-      if (!this.editableColumns.length) return;
-      // 至少一个单元格获得焦点
-      if (!this.clicked.length) return;
-      const { keyCode } = ev;
-      // 左  右
-      if (keyCode === 37 || keyCode === 39) {
-        ev.preventDefault();
-        const total = this.editableColumns.length;
-        let index = this.editableColumns.findIndex(x => x.dataIndex === this.clicked[1]);
-        let yIndex = keyCode === 37 ? (--index + total) % total : ++index % total;
-        const dataIndex = this.editableColumns[yIndex].dataIndex;
-        this.setClickedValues([this.clicked[0], dataIndex]);
-        this.scrollXToPosition(dataIndex);
-      }
-      // 上  下
-      if (keyCode === 38 || keyCode === 40) {
-        ev.preventDefault();
-        const { tableFullData, getRowKey } = this.$$table;
-        const total = tableFullData.length;
-        let index = tableFullData.findIndex(x => getRowKey(x) === this.clicked[0]);
-        let xIndex = keyCode === 38 ? (--index + total) % total : ++index % total;
-        const rowKey = getRowKey(tableFullData[xIndex]);
-        this.setClickedValues([rowKey, this.clicked[1]]);
-        this.scrollYToPosition(rowKey, xIndex);
-      }
-      // Esc
-      if (keyCode === 27) {
-        this.setClickedValues([]);
-      }
-    },
-    scrollXToPosition(dataIndex, index) {
-      const { leftFixedColumns } = this.$$table;
-      const v = _.isUndefined(index) ? this.flattenColumns.findIndex(x => x.dataIndex === dataIndex) : index;
-      if (v < 0) return;
-      const fixedWidth = leftFixedColumns.map(x => x.width || x.renderWidth || config.defaultColumnWidth).reduce((prev, curr) => prev + curr, 0);
-      this.$el.scrollLeft = this.$vTableBody.querySelectorAll('tbody > tr > td')[v].offsetLeft - fixedWidth;
-    },
-    scrollYToPosition(rowKey, index) {
-      const { tableFullData, scrollYStore, getRowKey } = this.$$table;
-      const v = _.isUndefined(index) ? tableFullData.findIndex(x => getRowKey(x) === rowKey) : index;
-      if (v < 0) return;
-      this.$el.scrollTop = v * scrollYStore.rowHeight;
     },
     renderBodyYSpace() {
       return <div class="v-body--y-space" />;
